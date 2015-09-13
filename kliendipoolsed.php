599d1ceae8ecae1e70c87ba83bffbff6abb2e3cf
Raul <raul.muser@gmail.com>
2015-09-14 00:59:21 +0300

    Lisa dokumenti pilt ilusast kassist ja tee nii, et kui pilti klikitakse, siis muutub kassipilt koerapildiks [Delivers #102937876]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -8,6 +8,8 @@
     <button onclick="tereMaailm()">Tere Maailm</button>
     <a href="javascript:alertKHK()">Tere Maailm</a>
     <a href="http://www.khk.ee" onclick="alert('Jääme siia!');return false;">Jääme siia</a>
+    <br>
+    <img id="pilt" onclick="asendaKoer()" width="200" height="140" src="http://s.hswstatic.com/gif/whiskers-sam.jpg">
 <script>
     function tereMaailm(){
         alert('Tere, Maailm!');
@@ -15,6 +17,11 @@
     function alertKHK(){
         alert('Tere, Maailm!');
         window.location="http://www.khk.ee";
+    }
+    function asendaKoer(){
+        document.addEventListener("click",function(){
+            document.getElementById("pilt").src="http://etaarifa.co.ke/wp-content/uploads/2015/08/black-dog-pics-765x510.jpg";
+        });
     }
 </script>
 </body>
