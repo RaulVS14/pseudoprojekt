7ad2194f3bc7fe122af10c8e83792eea231af1cd
Raul <raul.muser@gmail.com>
2015-09-15 06:53:33 +0300

    Luua fail nimega index.php. Lisada sinna html5 boilerplate. <body> osasse lisada PHP märgendi vahele kontroll, kas eksisteerib muutuja nimega $_GET['page'] ja kui eksisteerib, siis require'ida fail, mille nimeks on selle $_GET['page'] sisu pluss string ".php". Näiteks kui $_GET['page'] väärtuseks oli home, siis requiretakse home.php. [Delivers #103339744]

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-Tere, maailm!
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Serveripoolsed</title>
+</head>
+<body>
+<?php
+    if(isset($_GET['page'])){
+        require $_GET['page'].".php";
+    }
+?>
+</body>
+</html>
+
