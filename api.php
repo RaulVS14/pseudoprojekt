87b3e7a47115173aca68fd40d95a21d6de2c81d9
Raul <raul.muser@gmail.com>
2015-10-15 12:57:35 +0300

    34. CSV lugemine [Delivers #105678482]

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+    $filename="data.csv";
+    if(file_exists($filename)){
+        $file=fopen($filename,"r");
+        $andmed=[];
+        while(($row=fgetcsv($file))!==false){
+            $andmed[]=explode(";",$row[0]);
+        }
+        $json=json_encode($andmed, JSON_FORCE_OBJECT);
+        echo $json;
+        fclose($file);
+    }else{
+        echo "Faili ei leitud. Luuakse data.csv fail!";
+        $file=fopen($filename,"w");
+        file_put_contents($filename,"Toomas;Test;toomas.test@email.com\nPriit;Proov;priit.proov@email.com\n");
+        fclose($file);
+    }
+?>
